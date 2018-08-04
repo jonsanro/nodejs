0f9b0026f74f641a1218d407fe55d7347e7208fd
Jon San Roman <jonsanromansarasua@gmail.com>
2018-08-04 19:53:00 +0200

    Creación de home que recibe parámetros del api

--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -1,64 +1,65 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const Anuncio = require('../models/Anuncio');
 
+const url = 'https://swapi.co/api/planets/1/';
 const { query, validationResult } = require('express-validator/check');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async(req, res, next) => {
 
-    const segundo = (new Date()).getSeconds();
 
-    res.locals.valor = '<script>alert("Bienvenid@ a la página de anuncios de Jon")</script>';
+    try {
+        // recuperar datos de entrada
+        const name = req.query.name;
+        const sell = req.query.sell;
+        const price = req.query.price;
+        const photo = req.query.photo;
+        const tag = req.query.tag;
+        const limit = parseInt(req.query.limit);
+        const skip = parseInt(req.query.skip);
+        const fields = req.query.fields;
+        const sort = req.query.sort;
 
-    //La vista se renderiza (calcula) EN EL SERVIDOR
-    res.render('index', {
-        condicion: {
-            segundo,
-            //!Es igual que segundo: segundo,
-            par: segundo % 2 === 0
-        },
-        users: [
-            { name: 'Smith', age: 20 },
-            { name: 'Thomas', age: 37 },
-            { name: 'Jones', age: 18 }
-        ]
-    });
+        // crear el filtro vacio
+        const filtro = {};
+
+        if (name) {
+            filtro.name = name;
+        }
+
+        if (sell) {
+            filtro.sell = sell;
+        }
+
+        if (price) {
+            filtro.price = price;
+        }
+
+        if (photo) {
+            filtro.photo = photo;
+        }
+
+        if (tag) {
+            filtro.tag = tag;
+        }
+
+        const anuncios = await Anuncio.listar(filtro, limit, skip, fields, sort);
+
+
+        res.render('index', {
+            anuncios
+        });
+        // si await falla, lanza una excepción --> throw new Exception()
+        res.json({ success: true, result: anuncios });
+    } catch (err) {
+        next(err);
+    }
 });
 
-router.get('/otrapagina', function(req, res, next) {
-    //?En un middleware podemos responder  
-    //res.send('ok');
-    //?O llamar a next
-    next(new Error('No permitido'));
 
-})
+//La vista se renderiza (calcula) EN EL SERVIDOR
 
-router.get('/paramenruta/:dato', (req, res, next) => {
-    const dato = req.params.dato;
-    res.send('ok, recibido dato: ' + dato)
-});
-
-router.get('/params/:id([0-9]+/piso/:piso/puerta/:puerta)', (req, res, next) => {
-
-    const id = req.params.dato;
-    res.send('ok, recibido dato: ' + dato)
-});
-
-
-router.get('/enquerystring', [ //Validations
-    query('talla').isNumeric().withMessage('Debe ser numérico')
-], (req, res, next) => {
-    //http://localhost:3000/enquerystring/?color=rojo&talla=M
-    console.log(req.query);
-    validationResult(req).throw(); //Pasa los errores de validación a next(err)
-
-    //Si la ejecución llega aquí, es que todos los parámetros son válidos
-    res.send('ok');
-});
-
-router.post('/enelbody', (req, res, next) => {
-    console.log('req.body', req.body);
-    res.send('ok');
-});
 
 module.exports = router;
